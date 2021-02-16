45c5ab0160b6479a410faf3bec703c6935eb66f9
kdeng00 <kundeng94@gmail.com>
2021-02-15 20:58:31 -0500

    Initial commit

---
+++ MyTestProg/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using MyTestLib;
+
+namespace MyTestProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MyTestProg");
+
+            var testClass = new MyTestClass();
+            var myList = new List<double>()
+            {
+                1, 1, 2, 3, 5, 8, 13
+            };
+
+            var sum = testClass.SumListValues(myList);
+
+            Console.WriteLine("Values: ");
+
+            foreach(var val in myList)
+            {
+                Console.Write($"{val} ");
+            }
+
+            Console.WriteLine($"\nSum: {sum}");
+        }
+    }
+}
